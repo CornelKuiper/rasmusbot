3a3753ef3a214432eb18eaef8f6c4e86e8e2542e
Cornel <cornelk@live.nl>
2018-11-02 13:11:00 +0100

    standardising

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # rasmusbot
-Based on our lord and saviour
-Now also supports whatsapp messages. Just send a backup to your email and use this txt file as input.
-The code preach.py needs 4 arguments
-Argument 1: the filename (and the path if necessary) (so whatsapp_messages.txt)
-Argument 2: the length of the generated sentences
-Argument 3: the amount of generated sentences
-Argument 4: the minimum length of messages that are used for the chains (so if 5, messages with 5 words will not be used)
+Based on our lord and saviour<br/>
+Now also supports whatsapp messages. Just send a backup to your email and use this txt file as input.<br/>
+The code preach.py needs 5 arguments<br/>
+Argument 1: the mode: whapp, rasmus or trump<br/>
+Argument 2: the filename (and the path if necessary) (so whatsapp_messages.txt)<br/>
+Argument 3: the length of the generated sentences<br/>
+Argument 4: the amount of generated sentences<br/>
+Argument 5: the minimum length of messages that are used for the chains (so if 5, messages with 5 words will not be used)<br/>
--- preach.py
+++ preach.py
@@ -1,15 +1,12 @@
 import numpy as np
 import sys
-import random
 
-from collections import defaultdict, Counter
 from nltk.tokenize import TweetTokenizer
 
 def make_pairs(corpus):
     for i in range(len(corpus)-1):
         yield (corpus[i], corpus[i+1])
         
-
 def make_dict(pairs):
     word_dict = {}
     for word_1, word_2 in pairs:
@@ -18,7 +15,6 @@ def make_dict(pairs):
         else:
             word_dict[word_1] = [word_2]
     return word_dict
-
 
 def make_stuff(word_dict, corpus, n_words=20):
     first_word = np.random.choice(corpus)
@@ -49,11 +45,11 @@ def tokenize(lines, filterlen=5):
         line = filter_words2(line)
         if len(line) > filterlen:
             tokenized_lines.append(line)
-    print(len(tokenized_lines))
+    print(f"Amount of used lines: {len(tokenized_lines)}")
     return tokenized_lines
 
-def clean_input():
-    textfile = open('cleaned.txt','r')
+def clean_input(filename):
+    textfile = open(filename,'r')
     lines = textfile.read().split('},')
     extracted_lines = []
     for count, line in enumerate(lines):
@@ -62,6 +58,7 @@ def clean_input():
             split_line = " ".join(split_line[4:])
             if "\"url\"" not in split_line:
                 extracted_lines.append(split_line)  
+    textfile.close()
     return extracted_lines
 
 def clean_whatsapp(filename):
@@ -75,27 +72,40 @@ def clean_whatsapp(filename):
             split_line = split_line.split(':')
             split_line = " ".join(split_line[1:])
             if "omitted>" not in split_line:
-                extracted_lines.append(split_line)  
+                extracted_lines.append(split_line)
+    textfile.close()  
     return extracted_lines
 
-def main():
-    if not len(sys.argv)==5:
-        print("Incorrect amount of arguments, required: 4")
-        sys.exit(-1)
-    filename = sys.argv[1]
-    n_words = int(sys.argv[2])
-    n_lines = int(sys.argv[3])
-    filterlen = int(sys.argv[4])
+def get_trumped(filename, n_words, n_lines):
+    trump = open(filename, encoding='utf8').read()
+    corpus = trump.split()
 
-    # lines = clean_input()
-    lines = clean_whatsapp(filename)
+    pairs = make_pairs(corpus)
+    word_dict = make_dict(pairs)
+    for i in range(0,n_lines):
+        print(make_stuff(word_dict, corpus, n_words))
+
+def main():
+    if not len(sys.argv)==6:
+        print("Incorrect amount of arguments, required: 5")
+        sys.exit(-1)
+    mode = sys.argv[1]
+    filename = sys.argv[2]
+    n_words = int(sys.argv[3])
+    n_lines = int(sys.argv[4])
+    filterlen = int(sys.argv[5])
+    
+
+    if mode == 'trump':
+        get_trumped(filename, n_words, n_lines)
+        exit()     
+    elif mode == 'rasmus':
+        lines = clean_input(filename)
+    elif mode == 'whapp':
+        lines = clean_whatsapp(filename)
     tokenized_lines = tokenize(lines, filterlen)
     tokenized_lines = np.asarray(tokenized_lines)
     tokenized_lines = np.concatenate(tokenized_lines)
-
-    # trump = open('speeches.txt', encoding='utf8').read()
-    # corpus = trump.split()
-    # tokenized_lines = np.concatenate([tokenized_lines,corpus])
 
     pairs = make_pairs(tokenized_lines)
     word_dict = make_dict(pairs)
@@ -103,10 +113,9 @@ def main():
         print(make_stuff(word_dict, tokenized_lines, n_words))
         print("-----------------")
 
-    # pairs = make_pairs(corpus)
-    # word_dict = make_dict(pairs)
-    # for i in range(0,n_lines):
-    #     print(make_stuff(word_dict, corpus, n_words))
+
+
+
 
 if __name__ == "__main__":
     main()
