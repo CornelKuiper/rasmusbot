7fe9bc3c6557cfa857cfc5680edd212761e45244
Cornel <cornelk@live.nl>
2018-11-02 12:35:43 +0100

    whatsapp messages pipeline

--- preach.py
+++ preach.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
+import random
 
+from collections import defaultdict, Counter
 from nltk.tokenize import TweetTokenizer
 
 def make_pairs(corpus):
@@ -20,6 +22,8 @@ def make_dict(pairs):
 
 def make_stuff(word_dict, corpus, n_words=20):
     first_word = np.random.choice(corpus)
+    while first_word.islower():
+        first_word = np.random.choice(corpus)
 
     chain = [first_word]
 
@@ -37,13 +41,15 @@ def filter_words2(words):
     words2 = [word for word in words if (word.lower() not in stop_words)]
     return words2
 
-def tokenize(lines):
+def tokenize(lines, filterlen=5):
     tokenizer = TweetTokenizer()
     tokenized_lines = []
     for line in lines:
         line = tokenizer.tokenize(line)
         line = filter_words2(line)
-        tokenized_lines.append(line)
+        if len(line) > filterlen:
+            tokenized_lines.append(line)
+    print(len(tokenized_lines))
     return tokenized_lines
 
 def clean_input():
@@ -58,15 +64,32 @@ def clean_input():
                 extracted_lines.append(split_line)  
     return extracted_lines
 
-def main():
-    if not len(sys.argv)==3:
-        print("Incorrect amount of arguments, required: 1")
-        sys.exit(-1)
-    n_words = int(sys.argv[1])
-    n_lines = int(sys.argv[2])
+def clean_whatsapp(filename):
+    textfile = open(filename,'r')
+    lines = textfile.readlines()
+    extracted_lines = []
+    for count, line in enumerate(lines):
+        split_line = line.split('-')
+        if len(split_line)>1:
+            split_line = " ".join(split_line[1:])
+            split_line = split_line.split(':')
+            split_line = " ".join(split_line[1:])
+            if "omitted>" not in split_line:
+                extracted_lines.append(split_line)  
+    return extracted_lines
 
-    lines = clean_input()
-    tokenized_lines = tokenize(lines)
+def main():
+    if not len(sys.argv)==5:
+        print("Incorrect amount of arguments, required: 4")
+        sys.exit(-1)
+    filename = sys.argv[1]
+    n_words = int(sys.argv[2])
+    n_lines = int(sys.argv[3])
+    filterlen = int(sys.argv[4])
+
+    # lines = clean_input()
+    lines = clean_whatsapp(filename)
+    tokenized_lines = tokenize(lines, filterlen)
     tokenized_lines = np.asarray(tokenized_lines)
     tokenized_lines = np.concatenate(tokenized_lines)
 
@@ -84,7 +107,6 @@ def main():
     # word_dict = make_dict(pairs)
     # for i in range(0,n_lines):
     #     print(make_stuff(word_dict, corpus, n_words))
-
 
 if __name__ == "__main__":
     main()
